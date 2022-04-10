49ed1c415566348363683e2293107c4afe207c3b
dorrrn <doan7tran@gmail.com>
2022-04-10 18:11:05 +0200

    added drag and drop function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import initialData from './movies.json';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 function App() {
    const [movies, setMovies] = useState([]);
@@ -9,22 +10,49 @@ function App() {
       setMovies(initialData);
    }, []);
 
+   const handleOnDragEnd = (result) => {
+      if (!result.destination) return;
+      const items = Array.from(movies);
+      const [reorderedItem] = items.splice(result.source.index, 1);
+      items.splice(result.destination.index, 0, reorderedItem);
+      setMovies(items);
+   };
+
    const renderMovies = (list) => {
-      return list.map((movie) => {
+      return list.map((movie, index) => {
          return (
-            <li key={movie.id}>
-               <p>{movie.title}</p>
-               <p>{movie.year}</p>
-               <img src={movie.imgURL} alt={movie.title} />
-            </li>
+            <Draggable key={movie.title} draggableId={movie.title} index={index}>
+               {(provided) => {
+                  return (
+                     <li
+                        {...provided.draggableProps}
+                        {...provided.dragHandleProps}
+                        ref={provided.innerRef}
+                     >
+                        <p>{movie.title}</p>
+                        <p>{movie.year}</p>
+                        <img src={movie.imgURL} alt={movie.title} style={{ width: '20px' }} />
+                     </li>
+                  );
+               }}
+            </Draggable>
          );
       });
    };
 
    return (
-      <div className="App">
-         <h1>Hello</h1>
-         <ul>{renderMovies(initialData)};</ul>
+      <div className="App" style={{ display: 'flex', justifyContent: 'center' }}>
+         <DragDropContext onDragEnd={handleOnDragEnd}>
+            <Droppable droppableId="movies">
+               {(provided) => {
+                  return (
+                     <ul className="movies" {...provided.droppableProps} ref={provided.innerRef}>
+                        {renderMovies(movies)};{provided.placeholder}
+                     </ul>
+                  );
+               }}
+            </Droppable>
+         </DragDropContext>
       </div>
    );
 }
