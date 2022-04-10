323585244c37e2fac8268d19fd3481b7a3c2bd18
dorrrn <doan7tran@gmail.com>
2022-04-10 17:00:24 +0200

    render movies.json

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css';
+import { useEffect, useState } from 'react';
+import initialData from './movies.json';
 
 function App() {
+   const [movies, setMovies] = useState([]);
+
+   useEffect(() => {
+      setMovies(initialData);
+   }, []);
+
+   const renderMovies = (list) => {
+      return list.map((movie) => {
+         return (
+            <li key={movie.id}>
+               <p>{movie.title}</p>
+               <p>{movie.year}</p>
+               <img src={movie.imgURL} alt={movie.title} />
+            </li>
+         );
+      });
+   };
+
    return (
       <div className="App">
          <h1>Hello</h1>
+         <ul>{renderMovies(initialData)};</ul>
       </div>
    );
 }
